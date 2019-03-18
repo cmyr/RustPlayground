74c34619bdd030ecf5233b9881348221c1986dd2
Colin Rofls <colin@cmyr.net>
2019-03-18 12:34:25 -0400

    Initial project setup

---
+++ ModalInputTest/ModalInputTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ModalInputTest
+//
+//  Created by Colin Rofls on 2019-03-18.
+//  Copyright © 2019 Colin Rofls. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ModalInputTest/ModalInputTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ModalInputTest
+//
+//  Created by Colin Rofls on 2019-03-18.
+//  Copyright © 2019 Colin Rofls. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

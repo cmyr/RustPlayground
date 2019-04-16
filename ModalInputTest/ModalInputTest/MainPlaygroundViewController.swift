2d094ee7f56d8220e27b4b7cb7429f86cad7040b
Colin Rofls <colin@cmyr.net>
2019-04-16 11:07:24 -0400

    Add animated toggling of bottom view

--- ModalInputTest/ModalInputTest/MainPlaygroundViewController.swift
+++ ModalInputTest/ModalInputTest/MainPlaygroundViewController.swift
@@ -13,7 +13,37 @@ class MainPlaygroundViewController: NSSplitViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         splitViewItems[1].isCollapsed = true
-        // Do view setup here.
+    }
+
+    var outputViewIsVisible: Bool = false {
+        didSet {
+            guard outputViewIsVisible != oldValue else { return }
+            let isVisible = outputViewIsVisible
+            if isVisible {
+                self.splitViewItems[1].isCollapsed = false
+            }
+            // adapted from https://stackoverflow.com/questions/26664759/how-to-collapse-an-nssplitview-pane-with-animation-while-using-auto-layout
+            NSAnimationContext.runAnimationGroup({ (context) in
+                context.allowsImplicitAnimation = true
+                context.duration = 0.25
+                if isVisible {
+                    self.splitView.setPosition(self.view.frame.height - 140, ofDividerAt: 0)
+                } else {
+                    self.splitView.setPosition(self.view.frame.height, ofDividerAt: 0)
+                }
+                self.splitView.layoutSubtreeIfNeeded()
+            }) {
+                if !isVisible {
+                    self.splitViewItems[1].isCollapsed = true
+                }
+            }
+        }
+    }
+
+
+    @IBAction func toggleOutputView(_ sender: NSButton?) {
+//        let button = sender as! NSButton
+        outputViewIsVisible = sender!.state == .on
     }
 
     @IBAction func build(_ sender: Any?) {

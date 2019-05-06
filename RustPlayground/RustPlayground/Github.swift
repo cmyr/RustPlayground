66e39b496e0fc2ebe5321be9e94e8405e0ea8dd7
Colin Rofls <colin@cmyr.net>
2019-05-05 21:58:56 -0400

    More thorough github auth error handling

--- RustPlayground/RustPlayground/Github.swift
+++ RustPlayground/RustPlayground/Github.swift
@@ -14,6 +14,8 @@ fileprivate let HTTP_HEADER_GITHUB_SCOPES = "X-OAuth-Scopes"
 enum GithubError {
     /// The request returned an error
     case Connection(Error)
+    case InvalidResponse
+    case NotAuthorized
     /// The token does not have the 'gists' authorization
     case MissingGistAuthorization
     /// The request returned okay, but was missing an expected header
@@ -25,6 +27,10 @@ extension GithubError: Error {
         switch self {
         case .Connection(let error):
             return "Failed to connect to github.com. '\(error.localizedDescription)'"
+        case .InvalidResponse:
+            return "Authorization failed, invalid response."
+        case .NotAuthorized:
+            return "Authorization failed, invalid token."
         case .MissingGistAuthorization:
             return "Authorization failed. The provided token does not have the 'gists' permission."
         case .MissingExpectedHeader:
@@ -50,20 +56,26 @@ class GithubConnection {
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             DispatchQueue.main.async {
                 if let error = error {
-                    completionHandler(.Connection(error))
-                    return
+                    return completionHandler(.Connection(error))
                 }
 
-                guard let response = response as? HTTPURLResponse, let authorizedScopes = response.allHeaderFields[HTTP_HEADER_GITHUB_SCOPES] as? String else {
-                    completionHandler(.MissingExpectedHeader)
-                    return
+                guard let response = response as? HTTPURLResponse else {
+                    return completionHandler(.InvalidResponse)
                 }
 
-                if authorizedScopes.contains("gist") {
-                    completionHandler(nil)
-                } else {
-                    completionHandler(.MissingGistAuthorization)
+                if response.statusCode != 200 {
+                    return completionHandler(.NotAuthorized)
                 }
+
+                guard let authorizedScopes = response.allHeaderFields[HTTP_HEADER_GITHUB_SCOPES] as? String else {
+                    return completionHandler(.MissingExpectedHeader)
+                }
+
+                if !authorizedScopes.contains("gist") {
+                    return completionHandler(.MissingGistAuthorization)
+                }
+
+                completionHandler(nil)
             }
         }.resume()
     }
